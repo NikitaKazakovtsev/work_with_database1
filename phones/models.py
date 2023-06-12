2580ab568883bf2c9fcbb5a32d67d4fc667f8a91
Kazakovtsev Nikita <nikita_kazakovtsev@bk.ru>
2023-06-12 13:00:20 +0300

    asdasd

--- phones/models.py
+++ phones/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Phone(models.Model):
     # TODO: Добавьте требуемые поля
     name = models.CharField(max_length=255, unique=True)
-    price = models.IntegerField(max_length=255)
-    image = models.URLField(max_length=255)
+    price = models.IntegerField()
+    image = models.URLField()
     release_date = models.DateField(max_length=255)
     lte_exists = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, primary_key=True)
